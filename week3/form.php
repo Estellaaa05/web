552bb2ba684ceead14ba82385bbc5e501424f070
Estella <waixin221008@dasein.edu.my>
2023-06-27 11:33:27 +0800

    exercise 1 done

--- week3/form.php
+++ week3/form.php
@@ -2,7 +2,7 @@
 
 <body>
 
-    <form action="welcome.php" method="get">
+    <form action="name.php" method="get">
         First Name:<input type="text" name="firstName"><br>
         Last Name:<input type="text" name="lastName"><br>
         <input type="submit">
