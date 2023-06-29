d98ae96ddf5ccb81f1b396512603803fd16b3885
Estella <waixin221008@dasein.edu.my>
2023-06-29 16:52:34 +0800

    Q3 done

---
+++ week3/sumForm.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+    <style>
+        .error {
+            color: #FF0000;
+        }
+    </style>
+</head>
+
+<body>
+
+    <body>
+
+        <form action="sumForm.php" method="get">
+            First Name:<input type="text" name="firstName"><br>
+            Last Name:<input type="text" name="lastName"><br>
+            <input type="submit">
+        </form>
+
+        <?php
+        if ($_GET) {
+            if (empty($_GET["firstName"]) || empty($_GET["lastName"]) || is_numeric($_GET["firstName"]) == false || is_numeric($_GET["lastName"]) == false) {
+                echo '<span class="error">Please fill in a number.</span>';
+            } else {
+                $sum = $_GET["firstName"] + $_GET["lastName"];
+                echo ($sum);
+            }
+        }
+        ?>
+
+    </body>
+
+</html>
