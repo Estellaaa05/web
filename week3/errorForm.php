96deb0ba5545837dd44818ebad5d0af37ca58ccc
Estella <waixin221008@dasein.edu.my>
2023-06-29 16:35:45 +0800

    Q2 done

---
+++ week3/errorForm.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+    <style>
+        .error {
+            color: #FF0000;
+        }
+    </style>
+</head>
+
+<body>
+
+    <body>
+
+        <form action="errorForm.php" method="get">
+            First Name:<input type="text" name="firstName"><br>
+            Last Name:<input type="text" name="lastName"><br>
+            <input type="submit">
+        </form>
+
+        <?php
+        if ($_GET) {
+            if (empty($_GET["firstName"]) || empty($_GET["lastName"])) {
+                echo '<span class="error">Please enter your name.</span>';
+            } else {
+                echo ucwords(strtolower($_GET["firstName"] . " "));
+                echo ucwords(strtolower($_GET["lastName"]));
+            }
+        }
+        ?>
+
+    </body>
+
+</html>
