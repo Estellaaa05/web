9415f33b7b1d750502e74f05f546c30914daa3c1
Estella <waixin221008@dasein.edu.my>
2023-06-29 17:31:16 +0800

    Q4 done

---
+++ week3/decForm.php
@@ -0,0 +1,39 @@
+<html>
+
+<head>
+    <style>
+        .error {
+            color: #FF0000;
+        }
+    </style>
+</head>
+
+<body>
+
+    <body>
+
+        <form action="decForm.php" method="get">
+            Number:<input type="text" name="number"><br>
+            <input type="submit">
+        </form>
+
+        <?php
+        if ($_GET) {
+            if (empty($_GET["number"]) || is_numeric($_GET["number"]) == false) {
+                echo '<span class="error">Please fill in a number.</span>';
+            } else {
+                $sum = 0;
+                $equation = '';
+                for ($x = $_GET["number"]; $x >= 1; $x--) {
+                    $sum += $x;
+                    $equation .= "$x + ";
+                }
+                $equation = rtrim($equation, ' + ');
+                echo "$equation = $sum";
+            }
+        }
+        ?>
+
+    </body>
+
+</html>
