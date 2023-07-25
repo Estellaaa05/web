985a0b5c15ea112bd03fc2c27db31a1cb9136527
Estella <waixin221008@dasein.edu.my>
2023-07-25 08:29:25 +0800

    project 1.1done

---
+++ project/customer_create.php
@@ -0,0 +1,117 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>PDO - Create a Record - PHP CRUD Tutorial</title>
+    <!-- Latest compiled and minified Bootstrap CSS (Apply your Bootstrap here -->
+</head>
+
+<body>
+    <!-- container -->
+    <div class="container">
+        <div class="page-header">
+            <h1>Create Customer</h1>
+        </div>
+
+        <!-- html form to create product will be here -->
+        <!-- PHP insert code will be here -->
+        <?php
+        if ($_POST) {
+            // include database connection
+            include 'config/database.php'; //connect to database
+            try {
+                // insert query
+                $query = "INSERT INTO customers SET username=:username, password=:password, first_name=:first_name, last_name=:last_name, gender=:gender, date_of_birth=:date_of_birth, registration_date_time=:registration_date_time, account_status=:account_status";
+                // prepare query for execution
+                $stmt = $con->prepare($query); //con connect database
+                // posted values
+                $username = htmlspecialchars(strip_tags($_POST['username']));
+                $password = htmlspecialchars(strip_tags($_POST['password']));
+                $first_name = htmlspecialchars(strip_tags($_POST['first_name']));
+                $last_name = htmlspecialchars(strip_tags($_POST['last_name']));
+                $gender = isset($_POST['gender']) ? htmlspecialchars(strip_tags($_POST['gender'])) : '';
+                $date_of_birth = htmlspecialchars(strip_tags($_POST['date_of_birth']));
+                // bind the parameters
+                $stmt->bindParam(':username', $username); //bindParam = put $name into :name
+                $stmt->bindParam(':password', $password);
+                $stmt->bindParam(':first_name', $first_name);
+                $stmt->bindParam(':last_name', $last_name);
+                $stmt->bindParam(':gender', $gender);
+                $stmt->bindParam(':date_of_birth', $date_of_birth);
+                // specify when this record was inserted to the database
+                $registration_date_time = date('Y-m-d H:i:s');
+                $stmt->bindParam(':registration_date_time', $registration_date_time);
+                $account_status = 'active';
+                $stmt->bindParam(':account_status', $account_status);
+
+                // Execute the query
+                if (empty($username) || empty($password) || empty($first_name) || empty($last_name) || empty($gender) || empty($date_of_birth)) {
+                    echo "<div class='alert alert-danger'>Unable to save record.</div>";
+                } else if ($stmt->execute()) {
+                    echo "<div class='alert alert-success'>Record was saved.</div>";
+                }
+            }
+
+            // show error
+            catch (PDOException $exception) {
+                die('ERROR: ' . $exception->getMessage());
+            }
+        }
+        ?>
+
+        <!-- html form here where the product information will be entered -->
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <table class='table table-hover table-responsive table-bordered'>
+                <tr>
+                    <td>Username</td>
+                    <td><input type='text' name='username' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td><input type='password' name='password' class='form-control'></textarea></td>
+                </tr>
+                <tr>
+                    <td>First Name</td>
+                    <td><input type='text' name='first_name' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Last Name</td>
+                    <td><input type='text' name='last_name' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Gender</td>
+                    <td>
+                        <div class="form-control">
+                            <input type="radio" id="female" name="gender" value="female">
+                            <label for="female">Female</label><br>
+                            <input type="radio" id="male" name="gender" value="male">
+                            <label for="male">Male</label>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Date Of Birth</td>
+                    <td><input type='date' name='date_of_birth' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type='submit' value='Save' class='btn btn-primary' />
+                    </td>
+                </tr>
+            </table>
+        </form>
+
+    </div>  <!-- end .container -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ project/product_create.php
@@ -0,0 +1,112 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>PDO - Create a Record - PHP CRUD Tutorial</title>
+    <!-- Latest compiled and minified Bootstrap CSS (Apply your Bootstrap here -->
+</head>
+
+<body>
+    <!-- container -->
+    <div class="container">
+        <div class="page-header">
+            <h1>Create Product</h1>
+        </div>
+
+        <!-- html form to create product will be here -->
+        <!-- PHP insert code will be here -->
+        <?php
+        if ($_POST) {
+            // include database connection
+            include 'config/database.php'; //connect去那个database
+            try {
+                // insert query
+                $query = "INSERT INTO products SET name=:name, description=:description, price=:price, promotion_price=:promotion_price, manufacture_date=:manufacture_date, expired_date=:expired_date, created=:created";
+                // prepare query for execution
+                $stmt = $con->prepare($query); //con是连接database的钥匙
+                // posted values
+                $name = htmlspecialchars(strip_tags($_POST['name']));
+                $description = htmlspecialchars(strip_tags($_POST['description']));
+                $price = htmlspecialchars(strip_tags($_POST['price']));
+                $promotion_price = htmlspecialchars(strip_tags($_POST['promotion_price']));
+                $manufacture_date = htmlspecialchars(strip_tags($_POST['manufacture_date']));
+                $expired_date = htmlspecialchars(strip_tags($_POST['expired_date']));
+                // bind the parameters
+                $stmt->bindParam(':name', $name); //bindParam = 把$name放进:name里面
+                $stmt->bindParam(':description', $description);
+                $stmt->bindParam(':price', $price);
+                $stmt->bindParam(':promotion_price', $promotion_price);
+                $stmt->bindParam(':manufacture_date', $manufacture_date);
+                $stmt->bindParam(':expired_date', $expired_date);
+                // specify when this record was inserted to the database
+                $created = date('Y-m-d H:i:s');
+                $stmt->bindParam(':created', $created);
+                // 前面都是在准备 最后在这里Execute the query
+                if (empty($name) || empty($description) || empty($price) || empty($promotion_price) || empty($manufacture_date) || empty($expired_date)) {
+                    echo "<div class='alert alert-danger'>Unable to save record.</div>";
+                } else if ($price < $promotion_price) {
+                    echo "<div class='alert alert-danger'>Promotion price must be cheaper than original price.</div>";
+                } else if (strtotime($expired_date) < strtotime($manufacture_date)) {
+                    echo "<div class='alert alert-danger'>Expired date must be later than manufacture date.</div>";
+                } else if ($stmt->execute()) {
+                    echo "<div class='alert alert-success'>Record was saved.</div>";
+                }
+            }
+            // show error
+            catch (PDOException $exception) {
+                die('ERROR: ' . $exception->getMessage());
+            }
+
+        }
+        ?>
+
+        <!-- html form here where the product information will be entered -->
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <table class='table table-hover table-responsive table-bordered'>
+                <tr>
+                    <td>Name</td>
+                    <td><input type='text' name='name' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Description</td>
+                    <td><textarea name='description' class='form-control'></textarea></td>
+                </tr>
+                <tr>
+                    <td>Price</td>
+                    <td><input type='text' name='price' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Promotion Price</td>
+                    <td><input type='text' name='promotion_price' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Manufacture Date</td>
+                    <td><input type='date' name='manufacture_date' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Expired Date</td>
+                    <td><input type='date' name='expired_date' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type='submit' value='Save' class='btn btn-primary' />
+                        <a href='index.php' class='btn btn-danger'>Back to read products</a>
+                    </td>
+                </tr>
+            </table>
+        </form>
+
+    </div>  <!-- end .container -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
